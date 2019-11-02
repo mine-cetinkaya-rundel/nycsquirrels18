7baf5b0ffce7a2f7c20f205ce847798bdd0e5284
lee-suddaby <leesuddaby@gmail.com>
2019-11-02 20:13:19 +0000

    Dates in tidied data set now stored in Date format

--- data-raw/dataprep.R
+++ data-raw/dataprep.R
@@ -11,8 +11,18 @@ squirrels <- read_csv(here::here("data-raw", "2018_Central_Park_Squirrel_Census_
   rename(
     long = x,
     lat = y
+  ) %>%
+  mutate(date = as.character(date)) %>%
+    mutate(
+      date = paste(
+        substr(date, 1, 2),
+        substr(date, 3, 4),
+        substr(date, 5, 8),
+        sep="/"
+      ) %>%
+    as.Date(format='%m/%d/%Y')
   )
 
 # save data --------------------------------------------------------------------
 
-use_data(squirrels, compress = "xz")
+use_data(squirrels, compress = "xz", overwrite = TRUE)
